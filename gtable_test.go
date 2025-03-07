36c7dc77bd158db4f2add18db150dfa40fd984cb
cainmusic <freeboynil@gmail.com>
2025-03-07 17:06:37 +0800

    使用Trae和DeepSeek-R1为项目增加了几个测试用例

--- gtable_test.go
+++ gtable_test.go
@@ -1,6 +1,7 @@
 package gtable
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -57,5 +58,40 @@ func TestVeryLongTitle(t *testing.T) {
 	table.AppendHead([]string{"id", "age", "height", "weight"})
 	table.AppendBody([]string{"1", "19", "167", "140"})
 	table.AppendBody([]string{"2", "22", "182", "202"})
+	table.PrintData()
+}
+
+func TestEmptyTable(t *testing.T) {
+	table := NewTable()
+	table.PrintData()
+}
+
+func TestLongContent(t *testing.T) {
+	table := NewTable()
+	table.AppendTitle("Long Content Test")
+	table.AppendHead([]string{"ID", "Description"})
+	table.AppendBody([]string{"1", "This is a very long description text to test automatic wrapping or truncation functionality"})
+	table.AppendBody([]string{"2", strings.Repeat("long-", 50) + "end"})
+	table.PrintData()
+}
+
+func TestSpecialCharacters(t *testing.T) {
+	table := NewTable()
+	table.AppendTitle("Special Characters Test")
+	table.AppendHead([]string{"Symbol", "Content"})
+	table.AppendBody([]string{"Quotes", "Contains \"double quotes\" and 'single quotes'"})
+	table.AppendBody([]string{"Newline", "Line1\nLine2"})
+	table.PrintData()
+}
+
+func TestInvalidInput(t *testing.T) {
+	table := NewTable()
+	// 测试空标题
+	table.AppendTitle("")
+	// 测试空表头
+	table.AppendHead([]string{"", ""})
+	// 测试不一致的列数
+	table.AppendBody([]string{"1", "2", "3"})
+	table.AppendBody([]string{"4"})
 	table.PrintData()
 }
